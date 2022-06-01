0fbe01831136819557a3808fe5787fecf5b1f5ca
callyco14 <callista.skhd@gmail.com>
2022-06-01 01:57:06 -0500

    landing page fix

--- src/Components/Landing/landing.js
+++ src/Components/Landing/landing.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9c08b412eaa0aad18e99da8d9466768d39806cb14698addead71a3e12854996
+oid sha256:03606905c279930f6d1402caf0b5fc742a5f78af0fa2ba939ae1e319774c8e6d
 size 1186
