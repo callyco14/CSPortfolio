d9908ac8694c96cce24930612daad9037586a2d1
callyco14 <callista.skhd@gmail.com>
2022-06-01 10:09:44 -0500

    fixed chronological order:

--- src/Containers/FeaturedWork.js
+++ src/Containers/FeaturedWork.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5584fe72b2499a8f7fc9ace1e7d41fbcbeab4fed7f2037ce352b33b7c1c574a8
-size 2483
+oid sha256:ef46cdc1ee30c49db4529732f1bd8d917d77d361683d8bfc6f90e312bcc7f1b4
+size 2485
