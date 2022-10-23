6be9572e962a4d8549f1859db9c2b7d73da10436
callyco14 <callista.skhd@gmail.com>
2022-10-23 18:59:38 -0500

    briteseed mask section added

--- src/Containers/Briteseed/Layout1.js
+++ src/Containers/Briteseed/Layout1.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e12786ac179e1f61e44fe372f43333388c6e4e1fd2b1bd254ee8c44addaa9ce1
-size 1891
+oid sha256:19fcc4679068b6353f32f86c19c63b72976491bf549dda5721a7f97078f4efe7
+size 8750
