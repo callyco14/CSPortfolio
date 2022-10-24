25c2d9b24049b12dbcaf13380c82d5d2b7bae59f
callyco14 <callista.skhd@gmail.com>
2022-10-23 19:13:55 -0500

    oct resume

--- src/Screens/Resume/index.js
+++ src/Screens/Resume/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe5916455b7161a5108206893faad8508477992cccf29cb6fb6cb8904bb9d3cc
+oid sha256:9744528dfad54e8ff6e27155d8da62938a5223db94171cd8c3eb5d6d838e2ce3
 size 455
