c46002224e2b8f6c9b10e59feadfec7856d8cea1
callyco14 <callista.skhd@gmail.com>
2022-12-16 22:05:38 -0600

    briteseed hsi tool section completed

--- src/Containers/Briteseed/Layout1.js
+++ src/Containers/Briteseed/Layout1.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19fcc4679068b6353f32f86c19c63b72976491bf549dda5721a7f97078f4efe7
-size 8750
+oid sha256:832dd60dca0135a1ed73dac43976d3ad4e63ad37be18d08ba4140e35f19bf80b
+size 15430
