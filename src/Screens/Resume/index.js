6a61fdcfdcafe1f05a767d5606b4dc5af3c78f73
callyco14 <callista.skhd@gmail.com>
2022-12-16 20:12:56 -0600

    new resume

--- src/Screens/Resume/index.js
+++ src/Screens/Resume/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9744528dfad54e8ff6e27155d8da62938a5223db94171cd8c3eb5d6d838e2ce3
+oid sha256:e8d4a777c83f5a45109bdc7dcdf159e5e507195f186bd4c5929cbdd2f34d1766
 size 455
